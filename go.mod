14a3a734460e4ddb557049185a9647474825d799
crhistian lucumi <crhistianlucumi@iMac-de-crhistian.local>
2025-08-24 20:46:41 -0500

    v1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CrhistianDLM/cdlm_my_go_module
+
+go 1.25.0
---
+++ interfaces.go
@@ -0,0 +1,13 @@
+package figuras
+
+import "fmt"
+
+type Figura interface {
+	area() float64
+	perimetro() float64
+}
+
+func Medida(f Figura) {
+	fmt.Println(f)
+	fmt.Printf("Area: %.2f, Perimetro: %.2f\n", f.area(), f.perimetro())
+}
